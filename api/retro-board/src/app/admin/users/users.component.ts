e34a867d8d6aa9cc14dc32f6844bcfdb819a7a93
Michał Jagusiak <michaljagusiak@gmail.com>
2021-10-16 11:21:28 +0200

    poprawione fontawesome, profil

--- api/retro-board/src/app/admin/users/users.component.ts
+++ api/retro-board/src/app/admin/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  userName:string = "Aleksandra Niewiadomska"
+  avatarLink:string = environment.avatarLink + this.userName.replace(' ', '+')
   constructor() { }
 
   ngOnInit(): void {
